06874e1109b9a8f25a0cfdf2994f508afd1b47ec
Sandro Keil <github@sandro-keil.de>
2016-01-13 10:17:21 +0100

    add support for symfony console >=2.5 and zend service-manager >= 2.7

--- config/dependencies.php
+++ config/dependencies.php
@@ -7,19 +7,20 @@
  * @license   https://github.com/proophsoftware/prooph-cli/blob/master/LICENSE.md New BSD License
  */
 
+
+use Zend\ServiceManager\Factory\InvokableFactory;
+
 return [
     'services' => [
         'config' => require __DIR__ . '/config.php',
     ],
-    'invokables' => [
-        \Prooph\Cli\Code\Generator\Aggregate::class => '\Prooph\Cli\Code\Generator\Aggregate',
-        \Prooph\Cli\Code\Generator\Command::class => '\Prooph\Cli\Code\Generator\Command',
-        \Prooph\Cli\Code\Generator\CommandHandler::class => '\Prooph\Cli\Code\Generator\CommandHandler',
-        \Prooph\Cli\Code\Generator\CommandHandlerFactory::class => '\Prooph\Cli\Code\Generator\CommandHandlerFactory',
-        \Prooph\Cli\Code\Generator\Event::class => '\Prooph\Cli\Code\Generator\Event',
-        \Prooph\Cli\Console\Command\GenerateAll::class => \Prooph\Cli\Console\Command\GenerateAll::class,
-    ],
     'factories' => [
+        \Prooph\Cli\Code\Generator\Aggregate::class => InvokableFactory::class,
+        \Prooph\Cli\Code\Generator\Command::class => InvokableFactory::class,
+        \Prooph\Cli\Code\Generator\CommandHandler::class => InvokableFactory::class,
+        \Prooph\Cli\Code\Generator\CommandHandlerFactory::class => InvokableFactory::class,
+        \Prooph\Cli\Code\Generator\Event::class => InvokableFactory::class,
+        \Prooph\Cli\Console\Command\GenerateAll::class => InvokableFactory::class,
         \Prooph\Cli\Console\Command\GenerateAggregate::class
             => '\Prooph\Cli\Console\Container\GenerateAggregateFactory',
         \Prooph\Cli\Console\Command\GenerateCommand::class => '\Prooph\Cli\Console\Container\GenerateCommandFactory',
